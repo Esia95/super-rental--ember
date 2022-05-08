deeeb4a6bdfdd4b44eb6f5116d415dffd7191785
Estera Nowak <esteranowak95@gmail.com>
2022-05-08 12:00:48 +0200

    master

--- config/environment.js
+++ config/environment.js
@@ -47,5 +47,8 @@ module.exports = function (environment) {
     // here you can enable a production-specific feature
   }
 
+  ENV.MAPBOX_ACCESS_TOKEN =
+    'pk.eyJ1IjoiZXNpYTk1IiwiYSI6ImNsMGpsaHQwNTAxdHQzZG9kNndpdTBxaTUifQ.ug7xAU3XGjvaykfM4CJEdQ';
+
   return ENV;
 };
